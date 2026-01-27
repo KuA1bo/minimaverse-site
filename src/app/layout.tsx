fac76a278fdfbec2225597c1b3e623251036ad3b
Alex K <197497785+KuA1bo@users.noreply.github.com>
2026-01-27 12:57:08 +0300

    feat: add /about and /partners to global navigation

---
+++ src/app/layout.tsx
@@ -0,0 +1,11 @@
+<nav className="mt-4">
+  <ul className="flex flex-wrap gap-4 text-blue-600">
+    <li><a href="/" className="hover:underline">Home</a></li>
+    <li><a href="/about" className="hover:underline">About</a></li>
+    <li><a href="/ecosystem" className="hover:underline">Ecosystem</a></li>
+    <li><a href="/developers" className="hover:underline">Developers</a></li>
+    <li><a href="/partners" className="hover:underline">Partners</a></li>
+    <li><a href="/timeline" className="hover:underline">Timeline</a></li>
+    <li><a href="/news" className="hover:underline">News</a></li>
+  </ul>
+</nav>
