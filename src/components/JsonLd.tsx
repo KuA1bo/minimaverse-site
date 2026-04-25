08080289d24588b4d15a1e0d0caa370dd034c5e1
Alex <xalexblack13@gmail.com>
2026-04-25 19:59:00 +0300

    seo: optimize partners page, fix link to /about, add CollectionPage schema & update date

---
+++ src/components/JsonLd.tsx
@@ -0,0 +1,15 @@
+// src/components/JsonLd.tsx
+// Reusable component for injecting JSON-LD structured data into the page head
+
+interface JsonLdProps {
+  data: Record<string, unknown>;
+}
+
+export default function JsonLd({ data }: JsonLdProps) {
+  return (
+    <script
+      type="application/ld+json"
+      dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }}
+    />
+  );
+}
